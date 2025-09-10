6f02d32f88b8d4b508e50168b172384c7b4fe35c
zmas云科技 <3675239063@qq.com>
2025-09-10 17:55:43 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 # src/plugins/makeplugin/__init__.py
 from .actions.makeplugin import MakePlugin  # 从actions目录导入动作类
 
-__all__ = ["MakePlugin"]  # 声明暴露的类（框架通过__all__识别插件动作）
+
+__all__ = ["PluginGenerator"]  # 声明暴露的类（框架通过__all__识别插件动作）
